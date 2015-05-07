1c6f30602a2d186578dafd569d30e74ab1651ae6
Matt Ickstadt <matt@icky.pw>
2015-05-07 18:10:56 -0500

    Initial Commit

---
+++ Petri.Web/app.js
@@ -0,0 +1,36 @@
+﻿var PetriGame = (function () {
+    function PetriGame() {
+        this.game = new Phaser.Game("100%", "100%", Phaser.AUTO, 'content', {
+            preload: this.preload,
+            create: this.create,
+            update: this.update
+        });
+    }
+    PetriGame.prototype.preload = function () {
+    };
+
+    PetriGame.prototype.create = function () {
+    };
+
+    PetriGame.prototype.update = function () {
+    };
+
+    PetriGame.prototype.resize = function (width, height) {
+        this.game.width = width;
+        this.game.height = height;
+        this.game.stage.bounds.width = width;
+        this.game.stage.bounds.height = height;
+    };
+    return PetriGame;
+})();
+
+var game;
+
+window.onload = function () {
+    game = new PetriGame();
+};
+
+window.onresize = function () {
+    game.resize(window.innerWidth, window.innerHeight);
+};
+//# sourceMappingURL=app.js.map
---
+++ Petri.Web/app.ts
@@ -0,0 +1,42 @@
+﻿class PetriGame {
+
+    constructor() {
+        this.game = new Phaser.Game("100%", "100%", Phaser.AUTO, 'content', {
+            preload: this.preload,
+            create: this.create,
+            update: this.update
+        });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+
+    resize(width, height) {
+        this.game.width = width;
+        this.game.height = height;
+        this.game.stage.bounds.width = width;
+        this.game.stage.bounds.height = height;
+    }
+
+}
+
+var game;
+
+window.onload = () => {
+    game = new PetriGame();
+};
+
+window.onresize = () => {
+    game.resize(window.innerWidth, window.innerHeight);
+};
