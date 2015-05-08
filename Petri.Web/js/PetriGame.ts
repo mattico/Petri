af7f8da8bd2b69ced18e76da11fc3a70b9142e5c
Matt Ickstadt <matt@icky.pw>
2015-05-08 14:58:30 -0500

    Finally understand how it works.

---
+++ Petri.Web/js/PetriGame.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="../lib/definitions/phaser.d.ts"/>
+
+class PetriGame extends Phaser.Game {
+
+    button: Phaser.Button;
+
+    constructor() {
+        super(window.innerWidth, window.innerHeight, Phaser.AUTO, "", null);
+        this.state.add("PreGame", new PreGameState());
+        this.state.add("Game", new GameState());
+        this.state.add("PostGame", new PostGameState());
+
+        this.state.start("PreGame");
+    }
+
+    resize(width, height) {
+        this.width = width;
+        this.height = height;
+
+        this.world.width = width;
+        this.world.height = height;
+
+        if (game.renderType === Phaser.WEBGL) {
+            game.renderer.resize(width, height);
+        }
+    }
+} 
---
+++ Petri.Web/js/states/PreGameState.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="../../lib/definitions/phaser.d.ts"/>
+
+class PreGameState extends Phaser.State {
+
+    button: Phaser.Button;
+    text: Phaser.Text;
+    circle: Phaser.Circle;
+
+    preload() {
+     
+    }
+
+    create() {
+        this.world.bounds.width = conf.WorldWidth;
+        this.world.bounds.height = conf.WorldHeight;
+
+        game.resize(window.innerWidth, window.innerHeight);
+
+        this.button = this.add.button(50, 50, "Stuff");
+        this.text = this.add.text(50, 80, "Text", { align: "center", fill: "#fff" });
+        this.circle = new Phaser.Circle(100, 100, 25);
+    }
+
+    update() {
+        
+    }
+
+}
+     
